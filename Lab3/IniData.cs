7e9e2aefaeb48c567ecf6f1777f105dfe9fc6107
DFirsa <firsa.denis@gmail.com>
2019-09-30 00:16:58 +0300

    have added class structure

---
+++ Lab3/IniData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Lab3
+{
+    public class IniData
+    {
+        private List<Dictionary<string, string>> Sections;
+
+        public IniData()
+        {
+            Sections = new List<Dictionary<string, string>>();
+        }
+    }
+}
---
+++ Lab3/Parser.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab3
+{
+    public static class Parser
+    {
+        public static IniData ParseIni(string path)
+        {
+            IniData iniData = new IniData();
+
+            
+
+            return iniData;
+        }
+
+        private static string DeleteComment(string line)
+        {
+            if (line.Contains(";"))
+                return line.Substring(0, line.IndexOf(';') + 1);
+            else return line;
+        }
+    }
+}
