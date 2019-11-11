6643c0b63221627a5e4bc20a4697121e8973bdff
DFirsa <firsa.denis@gmail.com>
2019-11-12 01:47:08 +0300

    have wrote file dao

--- Lab4/FileDAO.cs
+++ Lab4/FileDAO.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
+using System.Net;
 
 namespace Lab4
 {
@@ -24,14 +26,14 @@ namespace Lab4
             int id = -1;
             reader = File.OpenText(pathStores);
             string line;
-            
+
             while ((line = reader.ReadLine()) != null)
             {
                 string[] lines = line.Split(':');
                 lines[2].Trim();
                 if (lines[2].Equals(name.ToLower())) id = Int32.Parse(lines[1]);
             }
-            
+
             reader.Close();
             return id;
         }
@@ -46,40 +48,159 @@ namespace Lab4
                 sId = id;
                 id++;
             }
+
             writer.Close();
             return sId;
         }
 
-        public void CreateProduct(string productName, string store, float price)
+        private bool haveProduct(string product, int store_id)
         {
-            int storeId = CreateStore(store);
-            
-            
+            reader = File.OpenText(pathProducts);
+            string str;
+            bool result = false;
+            while ((str = reader.ReadLine()) != null)
+            {
+                string[] lines = str.Split(':');
+                lines[0].Trim();
+                lines[1].Trim();
+                if (Int32.Parse(lines[0]) == store_id && lines[1].Equals(product.ToLower()))
+                {
+                    result = true;
+                    break;
+                }
+            }
+
+            reader.Close();
+            return result;
         }
 
+        public void CreateProduct(string productName, string store, float price)
+        {
+            CreateProduct(productName, store, 0, price);
+        }
+
+        private void CreateProduct(string productName, string store, int count, float price)
+        {
+            int storeId = CreateStore(store);
+            if (haveProduct(productName, storeId)) return;
+
+            writer = File.AppendText(pathProducts);
+            writer.WriteLine($"{storeId} : {productName.ToLower()} : {count} : {price}");
+            writer.Close();
+        }
+
+        //TODO need to fix
         public void DeliverShipment(List<Shipment> shipments, string store)
         {
-            throw new System.NotImplementedException();
+            foreach (var shipment in shipments)
+            {
+                CreateProduct(shipment.product, store, shipment.count, shipment.price);
+            }
+        }
+
+        private string GetStoreById(int id)
+        {
+            reader = File.OpenText(pathStores);
+            string str;
+            while ((str = reader.ReadLine()) != null)
+            {
+                string[] lines = str.Split(':');
+                lines[0].Trim();
+
+                if (Int32.Parse(lines[0]) == id) return lines[1].Trim();
+            }
+
+            return null;
+        }
+
+        private List<string> GetStoresNamesById(List<int> id)
+        {
+            List<string> result = new List<string>();
+            foreach (var useID in id)
+            {
+                result.Add(GetStoreById(useID));
+            }
+
+            return result;
         }
 
         public List<string> FindCheapestProductStore(string productName)
         {
-            throw new System.NotImplementedException();
+            List<string> data = new List<string>();
+            reader = File.OpenText(pathProducts);
+
+            string str;
+            while ((str = reader.ReadLine()) != null)
+            {
+                string[] lines = str.Split(':');
+                lines[1].Trim();
+                if (lines[1].Equals(productName.ToLower()))
+                    data.Add($"{GetStoreById(Int32.Parse(lines[0].Trim()))}${lines[1]}");
+            }
+
+            reader.Close();
+
+            return data;
         }
 
         public List<string> GetProductsInfo(string store)
         {
-            throw new System.NotImplementedException();
+            int storeId = getStoreId(store);
+            reader = File.OpenText(pathProducts);
+            string str;
+
+            List<string> info = new List<string>();
+            while ((str = reader.ReadLine()) != null)
+            {
+                string[] lines = str.Split(':');
+                if (storeId == Int32.Parse(lines[0].Trim()))
+                    info.Add($"{lines[1].Trim()}${lines[3].Trim()}${lines[2].Trim()}");
+            }
+
+            reader.Close();
+            return info;
         }
 
         public void DecreaseCount(string product, string store, int count)
         {
-            throw new System.NotImplementedException();
+            int id = getStoreId(store);
+            reader = File.OpenText(pathProducts);
+
+            List<string> fileData = new List<string>();
+
+            string str;
+            while ((str = reader.ReadLine()) != null)
+            {
+                string[] lines = str.Split(':');
+                lines[0].Trim();
+                if (Int32.Parse(lines[0]) == id && lines[1].Trim().Equals(product.ToLower()))
+                    fileData.Add(
+                        $"{id} : {lines[1].Trim()} : {Int32.Parse(lines[2].Trim()) - count} : {lines[3].Trim()}");
+                else fileData.Add(str);
+            }
+            reader.Close();
+            
+            writer = new StreamWriter(pathProducts, false);
+            foreach (var line in fileData)
+            {
+                writer.WriteLine(line);
+            }
+            writer.Close();
         }
 
         public List<string> GetStoresList()
         {
-            throw new System.NotImplementedException();
+            List<string> result = new List<string>();
+            reader = File.OpenText(pathStores);
+
+            string str;
+            while ((str = reader.ReadLine()) != null)
+            {
+                string[] lines = str.Split(':');
+                result.Add(lines[1].Trim());
+            }
+            reader.Close();
+            return result;
         }
     }
 }
